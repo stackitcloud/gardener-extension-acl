3565bc7c9f497100eddbe189532dca3bd2ba57b4
Simon Kienzler <simon.kienzler@inovex.de>
2022-12-13 16:30:04 +0100

    Remove embarassing fmt.Printf() statement

--- pkg/webhook/webhook.go
+++ pkg/webhook/webhook.go
@@ -3,7 +3,6 @@ package webhook
 import (
 	"context"
 	"encoding/json"
-	"fmt"
 	"net/http"
 	"strings"
 
@@ -97,8 +96,6 @@ func (e *EnvoyFilterWebhook) createAdmissionResponse(
 		if err := controller.ConfigureProviderSpecificAllowedCIDRs(ctx, infra, &alwaysAllowedCIDRs); err != nil {
 			return admission.Errored(http.StatusInternalServerError, err)
 		}
-
-		fmt.Printf("\n\nAlways Allowed CIDRs: %+v\n\n", alwaysAllowedCIDRs)
 
 		filter, err := e.EnvoyFilterService.CreateInternalFilterPatchFromRule(extSpec.Rule, alwaysAllowedCIDRs)
 		if err != nil {
