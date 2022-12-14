82442a40f7b6bf368a37423592047a419754f13d
Simon Kienzler <simon.kienzler@inovex.de>
2022-12-14 13:43:42 +0100

    Fix deployment issues (go embed and RBAC)

--- charts/charts.go
+++ charts/charts.go
@@ -3,6 +3,9 @@ package charts
 import "embed"
 
 var (
-	//go:embed seed
+	// Go Embed happens to ignore files starting with _, e.g. `_helpers.tpl`.
+	// Using the `all` modifier for the embed directive fixes this.
+
+	//go:embed all:seed
 	Seed embed.FS
 )
